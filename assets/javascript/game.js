ef9bef2857fda8de15189bbce3226ce454527e5f
Derek Bennett <bnnder@netscape.net>
2019-06-29 23:09:46 -0700

    Add the css and javascript links to the html file to see changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,4 +5,13 @@ var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f',
                         's', 't', 'u', 'v', 'w', 'x',
                         'y', 'z'];
 
-                        
+// Game tracker variables
+var wins = 0;
+var losses = 0;
+var guesses = 0;
+var guessesLeft = 9;
+var guessedLetters = [];
+var letterToGuess = null;
+
+// Chooses a random letter
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
