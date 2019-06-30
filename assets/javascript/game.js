8b20bebced62cd3fc878b523236adbf26f183f1b
Derek Bennett <bnnder@netscape.net>
2019-06-29 22:50:20 -0700

    created variables for each letter of the alphabet

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// A to Z variables
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f',
+                        'g', 'h', 'i', 'j', 'k', 'l',
+                        'm', 'n', 'o', 'p', 'q', 'r',
+                        's', 't', 'u', 'v', 'w', 'x',
+                        'y', 'z'];
+
+                        
