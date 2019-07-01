60ee031090fbc4ce293cae9b21c91ee7de3c6570
Derek Bennett <bnnder@netscape.net>
2019-06-30 21:09:23 -0700

    Made some changees to the entire project from images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,59 @@
-// A to Z variables
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-// Game tracker variables
-var wins = 0;
-var losses = 0;
-var guesses = 0;
-var guessesLeft = 9;
+//define variables for game play
+var winCount = 1;
+var lossCount = 0;
+var guessesRemaining = 10;
 var guessedLetters = [];
-var letterToGuess = null;
 
-// Chooses a random letter
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+// set up array for computer to choose from
+// Easy play
+//  var alphabet =
+//    ["a","b","c","d","e"];
 
-// Updates the guess counter and selecting the HTML id guesses to track attempts
-var updateGuessesLeft = function() {
-    document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
-};
+// Difficult Play
+ var alphabet =
+     ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
+     ,"r","s","t","u","v","w","x","y","z"];
 
-//the keyword this. refers to the object
-var updateGuessesLeft = function() {
-    this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
-};
+// set up the computer to pick a random indexed value from aray
+var randomIndex = Math.floor(Math.random() * alphabet.length);
+var computerChoice = alphabet[randomIndex];
 
-// Displays the guesses attempted so far
-var updateGuessesSoFar = function() {
-    document.querySelector('#guesses').innerHTML =" Your Guesses so far!" + guessedLetters.join(', ');
-};
+console.log(computerChoice);
 
-// This is the reset function
-var reset = function() {
-    totalGuesses = 9;
-    guessesLeft = 9;
-    guessedLetters = [];
-+
-    updateLetterToGuess();
-    updateGuessesLeft();
-    updateGuessesSoFar();
-}
-
-updateLetterToGuess();
-updateGuessesLeft();
-
-// This is the onkeyup event once the key is released
+// add a listener for the user to press a key
 document.onkeyup = function(event) {
-    guessesLeft--;
-  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
-  guessedLetters.push(userGuess);
-  updateGuessesLeft();
-  updateGuessesSoFar();
-
-        if (guessesLeft > 0){
-             if (userGuess == letterToGuess){
-                 wins++;
-                 document.querySelector('#wins').innerHTML = "Wins: " + wins;
--                alert("Yes, the letter was " + computerGuess.toUpperCase());
-+                alert("Yes, you are psychic!");
-                 reset();
-             }
-         }else if(guessesLeft == 0){
-             // Then we will loss and we'll update the html to display the loss 
-             losses++;
-             document.querySelector('#losses').innerHTML = "Losses: " + losses;
--            alert("Sorry, the correct letter was " + computerGuess.toUpperCase() + " maybe try again?");
-+            alert("Sorry, you're not psychic, maybe try again?");
-             // Then we'll call the reset. 
-             reset();
-         }
-};
+  var userChoice = event.key;
+  
+    // reset computer choice if the user loses
+    if (guessesRemaining <= 0) {
+      lossCount++;
+      document.getElementById("lossCount").innerHTML = lossCount++;
+      console.log("You lost!");
+      alert("You lost!");
+      guessesRemaining = 10;
+      guessedLetters = [];
+      document.getElementById("guessedLetters").innerHTML = guessedLetters;
+      document.getElementById("guessesRemaining").innerHTML = 10;
+      randomIndex = Math.floor(Math.random() * alphabet.length);
+      computerChoice = alphabet[randomIndex];
+      console.log(computerChoice);
+    }
+    // compares the randomly selected computer choice and user choice
+    if (computerChoice === userChoice) {
+      console.log("You won!");
+      alert("You won!");
+      document.getElementById("winCount").innerHTML = winCount++;
+      guessedLetters = [];
+      document.getElementById("guessedLetters").innerHTML = guessedLetters;
+      randomIndex = Math.floor(Math.random() * alphabet.length);
+      computerChoice = alphabet[randomIndex];
+      console.log(computerChoice);
+      guessesRemaining = 10;
+      document.getElementById("guessesRemaining").innerHTML = 10;
+    } else {
+      console.log("Guess again!");
+      document.getElementById("guessesRemaining").innerHTML = guessesRemaining--;
+      guessedLetters.push(userChoice);
+      document.getElementById("guessedLetters").innerHTML = guessedLetters;
+    }
+}
